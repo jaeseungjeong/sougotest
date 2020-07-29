e690ec6f042980fbb3db5845c68b9ec1567cf648
jaeseungjeong <56663654+jaeseungjeong@users.noreply.github.com>
2020-07-29 21:47:44 +0900

    Update fiware_ActivePower.sh

--- fiware_ActivePower.sh
+++ fiware_ActivePower.sh
@@ -23,7 +23,7 @@ res=$(curl -s -X GET 'http://10.1.5.199:8666/STH/v1/contextEntities/type/SmartMe
 
 for i in `seq 0 $size`
 do
-context=$(jq -r '.contextResponses[0].contextElement' test16.json)
+context=$(echo $res | jq -r '.contextResponses[0].contextElement')
 value=$(echo $context | jq -r '.attributes[0].values['$i'].attrValue')
 asset=$(echo $context | jq '.id')
 DataCategory=$(echo $context | jq '.attributes[0].name')
